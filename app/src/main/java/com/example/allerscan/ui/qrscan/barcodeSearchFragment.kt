e3472fe5965f3f8d61c53ef2df2b5e71ec8b5154
Anna <akozi@illinois.edu>
2025-10-31 23:29:48 -0500

    Search Barcode Works

--- app/src/main/java/com/example/allerscan/ui/qrscan/barcodeSearchFragment.kt
+++ app/src/main/java/com/example/allerscan/ui/qrscan/barcodeSearchFragment.kt
@@ -8,6 +8,11 @@ import androidx.camera.view.PreviewView
 import androidx.fragment.app.Fragment
 import com.example.allerscan.databinding.FragmentBarcodeSearchBinding
 import com.example.allerscan.databinding.FragmentQrScanBinding
+import android.widget.Toast
+import com.example.allerscan.AllergenChecker
+import com.example.allerscan.BarcodeIngredientLookup
+import android.util.Log
+
 
 class barcodeSearchFragment : Fragment() {
     private var _binding: FragmentBarcodeSearchBinding? = null
@@ -25,13 +30,26 @@ class barcodeSearchFragment : Fragment() {
     }
 
     private fun setupClickListeners() {
-        binding.buttonSaveProfile.setOnClickListener {
+        binding.buttonBarcode.setOnClickListener {
             val barcode = binding.inputBarcode.text.toString()
-//            if (barcode.isNotBlank()) {
-//                //try to make ingredient call
-//            } else {
-//
-//            }
+            if (barcode.isNotBlank() && barcode.length == 12) {
+                //try to make ingredient call
+                val productIngredients = BarcodeIngredientLookup()
+                val allergenCheck = AllergenChecker()
+                productIngredients.lookupOpenFoodFacts(barcode) { ingredients ->
+                    if (ingredients.isEmpty()) {
+                        Log.e("barcodeSearchFragment", "Ingredients Not found")
+                    } else {
+                        Log.d("barcodeSearchFragment", "Ingredients Found: $ingredients")
+                        val ingredientList = productIngredients.parseIngredients(ingredients)
+                        val safe = allergenCheck.foodSafe(ingredientList)
+                        Toast.makeText(requireContext(), "Barcode Found", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(requireContext(), "Please Enter a Valid Barcode (Must be 12 digits)", Toast.LENGTH_SHORT).show()
+            }
+            binding.inputBarcode.getText().clear()
         }
     }
 
